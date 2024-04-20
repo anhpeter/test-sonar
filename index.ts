481d2148e427a984b47f9dd4a17584bf8443892a
Anh Tran <thoanganh@tma.com.vn>
2024-04-20 22:20:42 +0700

    test promise-return code another

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
-async function yourFunction(promises: Promise<unknown>[]): Promise<undefined> {
-  await Promise.allSettled(promises);
-}
+const testFunc = async (): Promise<undefined> => {};
+
+const main = async () => {
+  const result = await testFunc();
+  console.log(result);
+};
+main();
